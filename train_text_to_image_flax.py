3edaaa700ad119b79d89499d8c6b2a26a5530ac9
lodestone-rock <lodestone.rock@gmail.com>
2022-12-30 11:23:41 +0700

    setting os.environ

--- train_text_to_image_flax.py
+++ train_text_to_image_flax.py
@@ -388,7 +388,7 @@ def main():
         batch = shard(batch)
 
         return batch
-    n_workers = psutil.cpu_count(logical = False) / 8 - 1
+    n_workers = psutil.cpu_count(logical = False) / 8 - 2
     total_train_batch_size = args.train_batch_size * jax.local_device_count()
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset, shuffle=True, num_workers=n_workers, prefetch_factor=args.prefetch_image, collate_fn=collate_fn, batch_size=total_train_batch_size, drop_last=True, persistent_workers=True
