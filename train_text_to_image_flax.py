4bdd538118a4434954a242498de9250d5237d6dd
lodestone-rock <lodestone.rock@gmail.com>
2023-01-09 18:46:37 +0700

    rando print 7

--- train_text_to_image_flax.py
+++ train_text_to_image_flax.py
@@ -395,6 +395,7 @@ def main():
 
     def preprocess_train(examples):
         images = [image.convert("RGB") for image in examples[image_column]]
+        print(images[0])
         examples["pixel_values"] = [train_transforms(image) for image in images]
         examples["input_ids"] = tokenize_captions(examples)
 
